73eb409afacec1fbb61922b0c1148b8eb9d42394
Neel Bullywon <n2bullyw@uwaterloo.ca>
2021-11-20 18:25:35 -0500

    test

--- src/random_digit_generator.c
+++ src/random_digit_generator.c
@@ -1,17 +1,16 @@
-//Random Digit Generator
-//Made by Neel Bullywon
+// //Random Digit Generator
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
 
-int main(){
-    srand(time(NULL));
-    int num = 0;
-    //This is so that it generates a value with 8 digits only
-    for (int i = 0; i <= 7; i++)
-    {
-        num = (num * 10) + (rand() % 10);
-    }
-    printf(" %d \n ", num);
-}
+// int main(){
+//     srand(time(NULL));
+//     int num = 0;
+//     //This is so that it generates a value with 8 digits only
+//     for (int i = 0; i <= 7; i++)
+//     {
+//         num = (num * 10) + (rand() % 10);
+//     }
+//     printf(" %d \n ", num);
+// }
